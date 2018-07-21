964fa4f303ad90364df16567891b3dc0574bde89
Maxim Samoilov <nitive@icloud.com>
2018-07-21 22:02:51 +0300

    smarter url detection

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,17 @@
-import { startsWith } from 'ramda'
+import * as fs from 'fs'
+import { URL } from 'url'
 
 export function isURL(resourseLocation: string): boolean {
-  return startsWith('http', resourseLocation)
+  try {
+    // tslint:disable-next-line:no-unused-expression
+    new URL(resourseLocation)
+
+    if (fs.existsSync(resourseLocation)) {
+      return false
+    }
+
+    return true
+  } catch {
+    return false
+  }
 }
